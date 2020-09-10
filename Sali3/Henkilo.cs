2dda67bb550b3ce4d0880e9f5aad2481453ecb35
Samppa-code <41010@edu.raseko.fi>
2020-09-10 14:10:56 +0300

    Lisätty Jäsen-luokka

--- Sali3/Henkilo.cs
+++ Sali3/Henkilo.cs
@@ -6,7 +6,9 @@ namespace Sali3
 {
     class Henkilo
     {
+       
         // Määritellään kaikkien henkilöiden yhteiset ominaisuudet (kentät)
+        // Jäsen-luokka perii nämä ominaisuudet
         protected string etunimi;
         protected string sukunimi;
         protected string katuosoite;
@@ -33,6 +35,46 @@ namespace Sali3
             this.katuosoite = katu;
             this.postinumero = pnro;
             this.postitoimipaikka = paikka;
+        }
+
+        // Metodeja testaukseen
+        public void naytaHenkilo()
+        {
+            Console.WriteLine("Etunimi on " + this.etunimi);
+            Console.WriteLine("Sukunimi on " + this.sukunimi);
+            Console.WriteLine("Katuosoite on " + this.katuosoite);
+            Console.WriteLine("Postinumero on " + this.postinumero);
+            Console.WriteLine("Postitoimipaikka on " + this.postitoimipaikka);
+        }
+    }
+    class Jasen : Henkilo
+    {
+        // Jäsenolioiden erityisominausuudet, joita ei ole Henkilo-luokassa
+        protected string jasenid;
+        protected string sukupuoli;
+
+        // Muodostin kaikilla parametreillä
+        public Jasen(string etu, string suku, string katu, string pnro, string paikka, string id, string sukup)
+        {
+            this.etunimi = etu;
+            this.sukunimi = suku;
+            this.katuosoite = katu;
+            this.postinumero = pnro;
+            this.postitoimipaikka = paikka;
+            this.jasenid = id;
+            this.sukupuoli = sukup;
+        }
+
+        //Metodeja testaukseen
+        public void naytaJasen()
+        {
+            Console.WriteLine("Etunimi on " + this.etunimi);
+            Console.WriteLine("Sukunimi on " + this.sukunimi);
+            Console.WriteLine("Katuosoite on " + this.katuosoite);
+            Console.WriteLine("Postinumero on " + this.postinumero);
+            Console.WriteLine("Postitoimipaikka on " + this.postitoimipaikka);
+            Console.WriteLine("Jäsennumero on " + this.jasenid);
+            Console.WriteLine("Sukupuoli on " + this.sukupuoli);
         }
     }
 }
--- Sali3/Program.cs
+++ Sali3/Program.cs
@@ -8,9 +8,30 @@ namespace Sali3
         {
             Console.WriteLine("Testataan Henkilo-olion luontia");
 
+            /*----------------------------------------------------
+             | TESTAUS ALKAA TÄSTÄ                                 |
+             |---------------------------------------------------- */
+            
             // Testataan oletusmuodostin
 
-            Henkilo henkilo = new Henkilo()
+            Henkilo henkilo = new Henkilo();
+
+            // Testataan muodostin kaikilla parametreillä
+
+            Henkilo henkilo1 = new Henkilo("Mika", "Hakala", "Kotikatu 3", "20100", "Turku");
+
+
+            // Testataan Jäsen-olion muodostimen toiminta
+            Jasen jasen = new Jasen("Mika", "Vainio", "Ylänteentie 146", "23100", "Mynämäki", "123", "1");
+            // Katsotaan olion ominaisuuksia
+            Console.WriteLine("Testi oletusmuodostimelle");
+            henkilo.naytaHenkilo();
+            Console.WriteLine();
+            Console.WriteLine("Testi muodostimelle kaikilla prarametreilla");
+            henkilo1.naytaHenkilo();
+            Console.WriteLine();
+            Console.WriteLine("Testi jäsenen muodostimelle");
+            jasen.naytaJasen();
         }
     }
 }
