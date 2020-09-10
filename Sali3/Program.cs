0ce9fd4b7b53b1cb9ddb42de728bc7d9f0324f9b
Samppa-code <41010@edu.raseko.fi>
2020-09-10 15:56:13 +0300

    Lisätty luokkia

--- Sali3/Program.cs
+++ Sali3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Sali3
 {
     class Program
@@ -23,7 +24,14 @@ namespace Sali3
 
             // Testataan Jäsen-olion muodostimen toiminta
             Jasen jasen = new Jasen("Mika", "Vainio", "Ylänteentie 146", "23100", "Mynämäki", "123", "1");
-            // Katsotaan olion ominaisuuksia
+
+            // Testataan Trainer-olion muodostimen toiminta. huom. nimiavaruus Henkilosto
+            Henkilosto.Trainer trainer = new Henkilosto.Trainer("Jakke", "Jäynä", "Kotikatu 2", "21200", "Raisio", "Juoksumatto", "0447057425", "jakke.jayna@huuhaa.com");
+
+            // Testataan Punnitus-olion toimintaa
+            Punnitus punnitus = new Punnitus("10.9.2020", "70,5", "1,71");
+            
+            // Katsotaan olioiden ominaisuuksia
             Console.WriteLine("Testi oletusmuodostimelle");
             henkilo.naytaHenkilo();
             Console.WriteLine();
@@ -32,6 +40,14 @@ namespace Sali3
             Console.WriteLine();
             Console.WriteLine("Testi jäsenen muodostimelle");
             jasen.naytaJasen();
+            Console.WriteLine();
+            Console.WriteLine("Testi trainer muodostimelle");
+            trainer.naytaTrainer();
+            Console.WriteLine();
+            Console.WriteLine("Testi punnitus muodostimelle");
+            punnitus.naytaPunnitus();
+            Console.WriteLine("Painoindeksi (BMI-metodi) on " + punnitus.BMI());
+
         }
     }
 }
---
+++ Sali3/Trainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Ulkoinen nimiavaruus Henkilosto
+namespace Henkilosto
+{
+    //Trainer-luokka perii Henkilo-luokan ominaisuudet
+    class Trainer : Sali3.Henkilo
+    {
+        protected string ala;
+        protected string puhelin;
+        protected string email;
+
+        // Muodostin kaikilla parametreilla
+        public Trainer(string etu, string suku, string katu, string pnro, string paikka, string ala, string puh, string email)
+        {
+            this.etunimi = etu;
+            this.sukunimi = suku;
+            this.katuosoite = katu;
+            this.postinumero = pnro;
+            this.postitoimipaikka = paikka;
+            this.ala = ala;
+            this.puhelin = puh;
+            this.email = email;
+        }
+
+        //Testausmetodi, joka näyttää tiedot
+        public void naytaTrainer()
+        {
+            Console.WriteLine("Etunimi on " + this.etunimi);
+            Console.WriteLine("Sukunimi on " + this.sukunimi);
+            Console.WriteLine("Katuosoite on " + this.katuosoite);
+            Console.WriteLine("Postinumero on " + this.postinumero);
+            Console.WriteLine("Postitoimipaikka on " + this.postitoimipaikka);
+            Console.WriteLine("Ala on " + this.ala);
+            Console.WriteLine("Puhelin numero on " + this.puhelin);
+            Console.WriteLine("Sähköposti on " + this.email);
+        }
+    }
+}
